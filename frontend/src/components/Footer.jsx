8978b05520624743913b53c667e2d4f0d77f4211
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-05 17:12:41 -0500

    added improved footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,52 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="bg-gray-100 py-6 text-center text-gray-500 text-sm mt-12">
-    &copy; {new Date().getFullYear()} MySite. All rights reserved.
-  </footer>
+  
+<footer class="block">
+  <div class="py-16 md:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
+    <div class="flex-col flex items-center">
+      <a href="javascript:void(0);" class="mb-8 inline-block max-w-full text-black">
+        <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a94d6f4e6cf96_Group%2047874-3.png" alt="" class="inline-block max-h-10" />
+      </a>
+      <div class="text-center font-semibold">
+        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+          About
+        </a>
+        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+          Features
+        </a>
+        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+          Works
+        </a>
+        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+          Support
+        </a>
+        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+          Help
+        </a>
+      </div>
+      <div class="mb-8 mt-8 border-b border-gray-300 w-48"></div>
+      <div class="mb-12 grid-cols-4 grid-flow-col grid max-w-52 gap-8 mx-auto">
+        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945b4ae6cf7b_Vector-1.svg" alt="" class="inline-block" />
+        </a>
+        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945560e6cf77_Vector.svg" alt="" class="inline-block" />
+        </a>
+        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a940535e6cf7a_Vector-3.svg" alt="" class="inline-block" />
+        </a>
+        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9433a9e6cf88_Vector-2.svg" alt="" class="inline-block" />
+        </a>
+      </div>
+      <p class="text-sm sm:text-base">
+        © Copyright 2021. All rights reserved.
+      </p>
+    </div>
+  </div>
+</footer>
+
 );
 
 export default Footer; 
