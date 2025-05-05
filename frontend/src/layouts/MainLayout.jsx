786cdb67e79940d71dc39efc6de929bd2d3d2f06
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-05 17:42:02 -0500

    added cta component

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import CTA from '../components/CallToAction'
 
 export default function MainLayout({ children }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <CTA />
       <Footer />
     </>
   )
