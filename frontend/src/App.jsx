522115d9351a6c56c7871a39606c590d85098908
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-05 16:57:15 -0500

    added layout and moved components into main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,10 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Footer from './components/Footer'
-import Features from './components/Features'
-import ContactForm from './components/ContactForm'
-import './App.css'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home'
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen w-full flex flex-col bg-gray-50">
-      <Header />
-      <main className="flex-grow">
-        <Hero />
-        <About />
-        <Features />
-        <ContactForm />
-      </main>
-      <Footer />
-    </div>
+    <MainLayout>
+      <Home />
+    </MainLayout>
   )
 }
-
-export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import ContactForm from '../components/ContactForm'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <ContactForm />
+    </>
+  )
+} 
