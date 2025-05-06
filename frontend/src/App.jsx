7802e1345e4b187c491d68be12104d742dac5b7a
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-06 00:33:05 -0500

    fixed formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import AppRoutes from './routes/AppRoutes'
+
 
 export default function App() {
   return (
     <MainLayout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <AppRoutes />
     </MainLayout>
   )
 }
