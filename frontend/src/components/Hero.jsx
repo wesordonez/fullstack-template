023701fd0227681d75747bb68c6b93ab39f32138
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-05 21:35:58 -0500

    finalyzed hero section styling in dark and light modes

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline'
+import duHeroImage from '@/assets/images/du-hero.png'
 
 const Hero = () => (
-  <header>
+  <section id="hero">
 		  {/* Hero Container */}
 		  <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-8 md:py-20">
 		    {/* Component */}
@@ -9,29 +11,31 @@ const Hero = () => (
 		      {/* Hero Content */}
 		      <div className="flex flex-col">
 		        {/* Hero Title */}
-		        <h1 className="mb-4 text-4xl font-bold md:text-6xl md:leading-tight">
-		          The Website You Want Without The Dev Time.
+		        <h1 className="mb-4 text-4xl font-bold md:text-6xl md:leading-tight text-gray-900 dark:text-gray-100">
+		          The Ultimate Web Development Full Stack Template
 		        </h1>
-		        <p className="mb-6 max-w-lg text-sm text-gray-500 sm:text-xl md:mb-10 lg:mb-12">
-		          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-		          aliquam, purus sit amet luctus venenatis, lectus
+		        <p className="mb-6 max-w-lg text-sm text-gray-500 dark:text-gray-400 sm:text-xl md:mb-10 lg:mb-12">
+		          This is a full stack template that is built with Django, Tailwind CSS, React, and Vite. It is built for large scalable web development projects.
 		        </p>
 		        {/* Hero Button */}
 		        <div className="flex items-center">
-		          <a href="#" className="mr-5 items-center rounded-md bg-black px-6 py-3 font-semibold text-white md:mr-6 lg:mr-8">
+		          <a
+		            href="#"
+		            className="mr-5 items-center rounded-md px-6 py-3 font-semibold text-white md:mr-6 lg:mr-8 bg-accent dark:bg-accent hover:bg-white hover:text-accent dark:hover:bg-gray-900 dark:hover:text-accent border-2 border-accent dark:border-accent transition-colors"
+		          >
 		            Let's Talk
 		          </a>
-		          <a href="#" className="flex max-w-full items-center font-bold">
-		            <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a94bd85e6cf98_ArrowUpRight%20(4).svg" alt="" className="mr-2 inline-block max-h-4 w-5" />
-		            <p>View Showreel</p>
+		          <a href="https://github.com/wesordonez/fullstack-template" className="flex max-w-full items-center font-bold hover:text-accent dark:text-accent transition-colors">
+		            <ArrowUpRightIcon className="mr-2 inline-block max-h-4 w-5 text-gray-900 dark:text-accent group-hover:text-accent transition-colors" />
+		            <p>View Template</p>
 		          </a>
 		        </div>
 		      </div>
 		      {/* Hero Image */}
-		      <img src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074" alt="" className="inline-block h-full w-full max-w-2xl" />
+		      <img src={duHeroImage} alt="" className="inline-block h-full w-full max-w-2xl rounded-2xl" />
 		    </div>
 		  </div>
-		</header>
+	</section>
 );
 
 export default Hero; 
