6fd2a35e51e195042eeb4d425a3345558541a0a4
Wesley Ordoñez <wesordonez1@gmail.com>
2025-05-05 22:11:50 -0500

    styled footer with light and dark

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,47 +1,48 @@
 import React from 'react';
+import duLogo from '@/assets/logos/du-logo-full.png'
 
 const Footer = () => (
   
-<footer class="block">
-  <div class="py-16 md:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
-    <div class="flex-col flex items-center">
-      <a href="javascript:void(0);" class="mb-8 inline-block max-w-full text-black">
-        <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a94d6f4e6cf96_Group%2047874-3.png" alt="" class="inline-block max-h-10" />
+<footer id="footer" className="block bg-gray-50 dark:bg-gray-900">
+  <div className="py-16 md:py-20 mx-auto w-full max-w-7xl px-5 md:px-10">
+    <div className="flex-col flex items-center">
+      <a href="javascript:void(0);" className="mb-8 inline-block max-w-full text-black dark:text-gray-100">
+        <img src={duLogo} alt="" className="inline-block max-h-20 dark:invert" />
       </a>
-      <div class="text-center font-semibold">
-        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
+      <div className="text-center font-semibold">
+        <a href="hero" className="inline-block px-6 py-2 font-normal text-black dark:text-gray-100 transition hover:text-blue-600 dark:hover:text-blue-400">
+          Home
+        </a>
+        <a href="#" className="inline-block px-6 py-2 font-normal text-black dark:text-gray-100 transition hover:text-blue-600 dark:hover:text-blue-400">
           About
         </a>
-        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
-          Features
+        <a href="#contact" className="inline-block px-6 py-2 font-normal text-black dark:text-gray-100 transition hover:text-blue-600 dark:hover:text-blue-400">
+          Contact
         </a>
-        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
-          Works
+        <a href="#" className="inline-block px-6 py-2 font-normal text-black dark:text-gray-100 transition hover:text-blue-600 dark:hover:text-blue-400">
+          Privacy Policy
         </a>
-        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
-          Support
-        </a>
-        <a href="javascript:void(0);" class="inline-block px-6 py-2 font-normal text-black transition hover:text-blue-600">
-          Help
+        <a href="#" className="inline-block px-6 py-2 font-normal text-black dark:text-gray-100 transition hover:text-blue-600 dark:hover:text-blue-400">
+          Terms of Service
         </a>
       </div>
-      <div class="mb-8 mt-8 border-b border-gray-300 w-48"></div>
-      <div class="mb-12 grid-cols-4 grid-flow-col grid max-w-52 gap-8 mx-auto">
-        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
-          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945b4ae6cf7b_Vector-1.svg" alt="" class="inline-block" />
+      <div className="mb-8 mt-8 border-b border-gray-300 dark:border-gray-700 w-48"></div>
+      <div className="mb-12 grid-cols-4 grid-flow-col grid max-w-52 gap-8 mx-auto">
+        <a href="javascript:void(0);" className="mx-auto flex-col flex max-w-6 items-center justify-center text-black dark:text-gray-100">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945b4ae6cf7b_Vector-1.svg" alt="" className="inline-block filter dark:invert" />
         </a>
-        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
-          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945560e6cf77_Vector.svg" alt="" class="inline-block" />
+        <a href="javascript:void(0);" className="mx-auto flex-col flex max-w-6 items-center justify-center text-black dark:text-gray-100">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a945560e6cf77_Vector.svg" alt="" className="inline-block filter dark:invert" />
         </a>
-        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
-          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a940535e6cf7a_Vector-3.svg" alt="" class="inline-block" />
+        <a href="javascript:void(0);" className="mx-auto flex-col flex max-w-6 items-center justify-center text-black dark:text-gray-100">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a940535e6cf7a_Vector-3.svg" alt="" className="inline-block filter dark:invert" />
         </a>
-        <a href="javascript:void(0);" class="mx-auto flex-col flex max-w-6 items-center justify-center text-black">
-          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9433a9e6cf88_Vector-2.svg" alt="" class="inline-block" />
+        <a href="javascript:void(0);" className="mx-auto flex-col flex max-w-6 items-center justify-center text-black dark:text-gray-100">
+          <img src="https://assets.website-files.com/6458c625291a94a195e6cf3a/6458c625291a9433a9e6cf88_Vector-2.svg" alt="" className="inline-block filter dark:invert" />
         </a>
       </div>
-      <p class="text-sm sm:text-base">
-        © Copyright 2021. All rights reserved.
+      <p className="text-sm sm:text-base text-gray-700 dark:text-gray-300">
+        © Copyright 2025. All rights reserved by Dunosis LLC.
       </p>
     </div>
   </div>
